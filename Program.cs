4a3fcf917175ad9d11b476eed94f707e4b4181f1
Denys Shevel <denys.shevel16@gmail.com>
2022-12-07 21:07:07 +0100

    Initialize a project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KnuOopLab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
